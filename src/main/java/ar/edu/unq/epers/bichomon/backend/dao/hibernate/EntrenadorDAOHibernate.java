f3346cea78246d9292419850c11270cc6e0e4c69
VictorDegano <victordegano@gmail.com>
2018-10-11 14:42:10 -0300

    Commit wip campeones

--- src/main/java/ar/edu/unq/epers/bichomon/backend/dao/hibernate/EntrenadorDAOHibernate.java
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/dao/hibernate/EntrenadorDAOHibernate.java
@@ -6,6 +6,8 @@ import ar.edu.unq.epers.bichomon.backend.service.runner.Runner;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
+import java.util.List;
+
 public class EntrenadorDAOHibernate implements EntrenadorDAO {
 
     @Override
@@ -30,6 +32,15 @@ public class EntrenadorDAOHibernate implements EntrenadorDAO {
     {
         Session session = Runner.getCurrentSession();
         session.update(entrenador);
+    }
+
+    public List<Entrenador> campeones() {
+        Session session = Runner.getCurrentSession();
+//        String hql = "select bicho.especie from Bicho bicho where bicho.duenio is null group by bicho.especie order by count(bicho.especie)";
+        String hql = "Select dojo.campeon.bichoCampeon.duenio from Dojo dojo where dojo.campeon is not null group by dojo.campeon.fechaInicioDeCampeon order by count(dojo.campeon.bichoCampeon.duenio)";
+        Query<Entrenador> query = session.createQuery(hql,  Entrenador.class);
+        query.setMaxResults(10);
+        return query.getResultList();
     }
 
 }
---
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/service/Leaderboard/LeaderBoardServiceImplementation.java
@@ -0,0 +1,28 @@
+package ar.edu.unq.epers.bichomon.backend.service.Leaderboard;
+
+import ar.edu.unq.epers.bichomon.backend.model.entrenador.Entrenador;
+import ar.edu.unq.epers.bichomon.backend.model.especie.Especie;
+
+import java.util.List;
+
+public class LeaderBoardServiceImplementation implements LeaderBoardService
+{
+    public LeaderBoardServiceImplementation() {
+    }
+
+    @Override
+    public List<Entrenador> campeones()
+    {
+        return null;
+    }
+
+    @Override
+    public Especie especieLider() {
+        return null;
+    }
+
+    @Override
+    public List<Entrenador> lideres() {
+        return null;
+    }
+}
