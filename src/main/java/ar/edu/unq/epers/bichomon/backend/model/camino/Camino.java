c471fce6f995d4cb921a7c394c47b581c13c6931
VictorDegano <victordegano@gmail.com>
2018-11-02 17:37:48 -0300

    Comienzo de UbicacionDAONEO4J

---
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/model/camino/Camino.java
@@ -0,0 +1,11 @@
+package ar.edu.unq.epers.bichomon.backend.model.camino;
+
+import ar.edu.unq.epers.bichomon.backend.model.ubicacion.Ubicacion;
+
+public class Camino
+{
+    private Ubicacion desdeUbicacion;
+    private Ubicacion hastaUbicacion;
+    private TipoUbicacion tipo;
+
+}
---
+++ src/main/java/ar/edu/unq/epers/bichomon/backend/model/camino/TipoUbicacion.java
@@ -0,0 +1,8 @@
+package ar.edu.unq.epers.bichomon.backend.model.camino;
+
+public enum TipoUbicacion
+{
+    TERRESTRE,
+    MARITIMO,
+    AEREO
+}
